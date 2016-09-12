5994977f610c5c8f15776583ce4729577bb191b0
ThiloZiel <thilo.ziel@outlook.de>
2016-09-12 11:09:14 +0200

    buttons main

--- app/src/main/java/android/mi/ur/de/urfit/MainActivity.java
+++ app/src/main/java/android/mi/ur/de/urfit/MainActivity.java
@@ -2,8 +2,16 @@ package android.mi.ur.de.urfit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button startButton = (Button) findViewById(R.id.startButton);
+    Button ergebnisButton = (Button) findViewById(R.id.ergebnisButton);
+    Button hilfeButton = (Button) findViewById(R.id.hilfeButton);
+    Button einstellungsButton = (Button) findViewById(R.id.einstellungsButton);
+    Button profilButton = (Button) findViewById(R.id.profilButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
