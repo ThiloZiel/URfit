e09cc5c2a77d86ddd2f6bccc16b8fc61fce7fb5b
DanielSchmaderer <danielschmaderer95@gmail.com>
2016-09-12 12:21:39 +0200

    Test

--- app/src/main/java/android/mi/ur/de/urfit/start_activity.java
+++ app/src/main/java/android/mi/ur/de/urfit/start_activity.java
@@ -9,5 +9,6 @@ public class start_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_activity);
+        int a = 2;
     }
 }
