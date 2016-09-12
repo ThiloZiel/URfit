280815ae85bb8f6ff3ef4703c18dc104dd8e28aa
DanielSchmaderer <danielschmaderer95@gmail.com>
2016-09-12 14:43:40 +0200

    URFitItem

---
+++ app/src/main/java/android/mi/ur/de/urfit/URFitItem.java
@@ -0,0 +1,41 @@
+package android.mi.ur.de.urfit;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+public class URFitItem implements Comparable<URFitItem> {
+    private String steps;
+    private String calories;
+    private GregorianCalendar cal;
+
+    public URFitItem(String steps, String calories, int day, int month, int year){
+        this.steps = steps;
+        this.calories = calories;
+        cal = new GregorianCalendar(year,month,day);
+    }
+
+    public String getCalories(){
+        return calories;
+    }
+
+    public String getSteps(){
+        return steps;
+    }
+
+    public String getFormattedDate(){
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT, Locale.GERMANY);
+
+        return dateFormat.format(cal.getTime());
+    }
+
+    private Date getDueDate(){
+        return cal.getTime();
+    }
+
+    @Override
+    public int compareTo(URFitItem another) {
+        return getDueDate().compareTo(another.getDueDate());
+    }
+}
