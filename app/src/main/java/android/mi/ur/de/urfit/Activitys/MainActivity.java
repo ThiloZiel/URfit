1c50595bcd176466bca0404ef017991d690e5e51
ThiloZiel <thilo.ziel@outlook.de>
2016-09-30 12:28:39 +0200

    Datenbank

--- app/src/main/java/android/mi/ur/de/urfit/Activitys/MainActivity.java
+++ app/src/main/java/android/mi/ur/de/urfit/Activitys/MainActivity.java
@@ -18,8 +18,6 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity{
 
-    // zum rauslöschen
-
     private Button startButton;
     private Button ergebnisButton;
     private Button hilfeButton;
--- app/src/main/java/android/mi/ur/de/urfit/Activitys/profile_Activity.java
+++ app/src/main/java/android/mi/ur/de/urfit/Activitys/profile_Activity.java
@@ -1,15 +1,15 @@
 package android.mi.ur.de.urfit.Activitys;
 
+import android.mi.ur.de.urfit.Hilfsklassen.User;
 import android.mi.ur.de.urfit.R;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 
 public class profile_Activity extends AppCompatActivity {
-
-    // zum rauslöschen
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,50 +22,24 @@ public class profile_Activity extends AppCompatActivity {
         initProfilButton();
     }
 
-    private void updateList() {
-        //tasks.clear();
-        //tasks.addAll(db.getAllToDoItems());
-        //tasks_adapter.notifyDataSetChanged();
-    }
-
-    private void addProfil(String age, String gender, String size, String weight, String stepLength) {
-        //Date dueDate = getDateFromString(date);
-        //GregorianCalendar cal = new GregorianCalendar();
-        //cal.setTime(dueDate);
-
-        //ToDoItem newTask = new ToDoItem(task, cal.get(Calendar.DAY_OF_MONTH),
-        //        cal.get(Calendar.MONTH), cal.get(Calendar.YEAR));
-
-        //db.insertToDoItem(newTask);
-        updateList();
-    }
 
     private void initProfilButton() {
         Button addProfilButton = (Button) findViewById(R.id.profil_save);
         addProfilButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText ageEdit = (EditText) findViewById(R.id.editText_age);
-                EditText genderEdit = (EditText) findViewById(R.id.editText_gender);
-                EditText sizeEdit = (EditText) findViewById(R.id.editText_size);
-                EditText weightEdit = (EditText) findViewById(R.id.editText_weight);
+
+                EditText nameEdit = (EditText) findViewById(R.id.editText_name);
+                Switch genderEdit = (Switch) findViewById(R.id.edit_gender);
+                genderEdit.setTextOn("männlich");
+                genderEdit.setTextOff("weiblich");
                 EditText stepLengthEdit = (EditText) findViewById(R.id.editText_steplength);
-                String age = ageEdit.getText().toString();
-                String gender = genderEdit.getText().toString();
-                String size = sizeEdit.getText().toString();
-                String weight = weightEdit.getText().toString();
+                String name = nameEdit.getText().toString();
+                Boolean gender = genderEdit.isChecked();
                 String stepLength = stepLengthEdit.getText().toString();
 
-                if (stepLength.equals("")) {
-                } else {
-                    ageEdit.setText("");
-                    genderEdit.setText("");
-                    sizeEdit.setText("");
-                    weightEdit.setText("");
-                    stepLengthEdit.setText("");
+                MainActivity.dataSource.insertUser(new User(name,gender,stepLength));
 
-                    addProfil(age,gender,size,weight,stepLength);
-                }
             }
         });
     }
